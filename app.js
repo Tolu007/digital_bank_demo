e3085b7f77049432080d617b1fed4c5a40109674
Tolu ogundoyin <toluogundoyin007@gmail.com>
2022-08-16 15:45:57 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 // import { mongoose } from 'mongoose';
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(cors());
 
 
 
