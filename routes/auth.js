7e4fc6d6941faf2dbaff0d1aabdc75a5ab28400f
Tolu ogundoyin <toluogundoyin007@gmail.com>
2022-08-18 13:25:59 +0100

    initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const users = require('../models/userSchema');
 router.post('/', async (req, res) => {
     let user = await users.findOne({ number: req.body.number });
     if (!user) {
-        return res.status(400).send({ message: 'failed' });
+        return res.status(400).send({ message: 'Number not found' });
     }
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) {
